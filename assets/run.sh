acdf227172149974e39caf2dd03c2c44d7444c6e
Sankar Kanagarathinam <sankar.kanagarathinam@dowjones.com>
2019-06-10 16:34:25 -0400

    jmx added

--- assets/run.sh
+++ assets/run.sh
@@ -2,4 +2,4 @@
 
 python /app/init.py
 #exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
-/opt/activemq/bin/activemq console -Djava.rmi.server.hostname=1098
+/opt/activemq/bin/activemq console -Djava.rmi.server.hostname=0.0.0.0
