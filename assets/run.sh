6a514a6b11758b563dec43a2cc858936fbcf8848
Sankar Kanagarathinam <sankar.kanagarathinam@dowjones.com>
2019-06-10 21:56:18 -0400

    jmx added

--- assets/run.sh
+++ assets/run.sh
@@ -3,4 +3,4 @@
 python /app/init.py
 #exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 IP=$(hostname -i)
-/opt/activemq/bin/activemq console -Djava.rmi.server.hostname=$IP
+/opt/activemq/bin/activemq console -Djava.rmi.server.hostname=$IP -Dcom.sun.management.jmxremote.rmi.port=1098 -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote
