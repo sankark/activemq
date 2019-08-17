285fc2290797ca57bc1c172d34699be910824625
Sankar Kanagarathinam <sankar.kanagarathinam@dowjones.com>
2019-08-17 10:22:10 -0400

    ttl

--- assets/init.py
+++ assets/init.py
@@ -215,7 +215,7 @@ class ServiceRun():
           <networkConnector uri="%s"
             messageTTL="-1"
             consumerTTL="1"
-            prefetchSize="1"/>
+            prefetchSize="1000"/>
         </networkConnectors> \n""" % (connector_uri)
             self.replace_all(ACTIVEMQ_CONF + "/activemq.xml", '</broker>', rightManagement + '</broker>')
 
